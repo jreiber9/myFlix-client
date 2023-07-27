a667aba42b80af552d55cc2d9549861b0e3974c9
jreiber9 <128739362+jreiber9@users.noreply.github.com>
2023-07-27 17:32:34 -0400

    added setMovies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Interstellar",
-            description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
-            image:
-                "https://m.media-amazon.com/images/I/A1JVqNMI7UL._AC_UF894,1000_QL80_.jpg",
-            genre: "Sci-fi",
-            director: "Christopher Nolan"
-        },
-        {
-            id: 2,
-            title: "Jaws",
-            description: "When a killer shark unleashes chaos on a beach community off Cape Cod, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.",
-            image:
-                "https://m.media-amazon.com/images/I/71eMMDtyi8L._AC_UF894,1000_QL80_.jpg",
-            genre: "Thriller",
-            director: "Steven Spielberg"
-        },
-        {
-            id: 3,
-            title: "Hook",
-            description: "When Captain James Hook kidnaps his children, an adult Peter Pan must return to Neverland and reclaim his youthful spirit in order to challenge his old enemy.",
-            image:
-                "https://m.media-amazon.com/images/I/71qogNdrdFL._RI_.jpg",
-            genre: "Adventure",
-            director: "Steven Spielberg"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://myflixdbjr-b47a7be5f2e2.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.docs.map((doc) => {
+                    return {
+                        id: doc.key,
+                        title: doc.title,
+                        image:
+                            `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+                        author: doc.author_name?.[0]
+                    };
+                });
+
+                setMovies(moviesFromApi);
+            });
+    }, []);
 
     if (selectedMovie) {
         return (
