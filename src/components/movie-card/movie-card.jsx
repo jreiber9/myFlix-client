97c0486a3c8d6df5c18b000a1681eb9d0448a4db
jreiber9 <128739362+jreiber9@users.noreply.github.com>
2023-08-03 16:17:58 -0400

    Bootstrap sytling movieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,31 @@
 // Here you import the PropTypes library
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
+        <Card>
+            <Card.Img variant="top" src={movie.image} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>.
+                <Card.Text>{movie.Director.Name}</Card.Text>
+                <Button onClick={() => onMovieClick(Movie)} variant="link">
+                    Open
+                </Button>
+            </Card.Body>
+        </Card>
     );
 };
+
+{/* <div
+    onClick={() => {
+        onMovieClick(movie);
+    }}
+>
+    {movie.title}
+</div>
+    );
+}; */}
 
 // Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
